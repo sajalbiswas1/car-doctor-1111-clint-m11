76ffb4a8cc451a1abde94dfbe926def2d4c6476e
Programing Hero <sajalb1899@gmail.com>
2023-11-11 08:03:42 +0600

    data text search

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -5,19 +5,28 @@ import useAxiosApi from "../../../Hooks/useAxiosApi";
 
 
 const Services = () => {
-    const [asc,setAsc]= useState(true)
-    const [services,setServises]=useState([])
+    const [asc, setAsc] = useState(true)
+    const [services, setServises] = useState([])
+    const [search, setSearch] = useState('')
     // const [serviceSort, setServisesSort] = useState(asc)
     const axiosApi = useAxiosApi();
-    useEffect(()=>{
+    useEffect(() => {
         // fetch('http://localhost:5000/services')
         // .then(res => res.json())
         // .then(data => setServises(data))
         console.log(asc)
-        axiosApi(`services/?sort=${asc ? 'asc': 'desc' }`)
-        .then(res =>setServises(res.data))
+        axiosApi(`services/?sort=${asc ? 'asc' : 'desc'}&search=${search}`)
+            .then(res => setServises(res.data))
 
-    },[asc, axiosApi])
+    }, [asc, axiosApi,search])
+
+    const handleSearch = e => {
+        e.preventDefault()
+        const searchValue = e.target.text.value;
+        setSearch(searchValue)
+    }
+
+
     console.log(services)
     return (
         <div className="mt-10">
@@ -25,12 +34,16 @@ const Services = () => {
                 <h3 className="text-xl font-bold text-[#FF3811]">Service</h3>
                 <h1 className="text-5xl font-bold">Our Service Area</h1>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
-                <button onClick={()=>setAsc(!asc)} className="btn btn-success">Price: {asc ? "High to Low": "Low to high"}</button>
+                <form onSubmit={handleSearch}>
+                    <input className="border border-black p-2 mr-2 rounded-xl" type="text" name="text" id="" />
+                    <input type="submit" name="" value="Search" className="btn btn-accent" id="" />
+                </form>
+                <button onClick={() => setAsc(!asc)} className="btn btn-success">Price: {asc ? "High to Low" : "Low to high"}</button>
                 <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6">
-                {
-                    services.map(service => <ServicesCard key={service._id} service={service}></ServicesCard>)
-                }
-                
+                    {
+                        services.map(service => <ServicesCard key={service._id} service={service}></ServicesCard>)
+                    }
+
                 </div>
             </div>
         </div>
